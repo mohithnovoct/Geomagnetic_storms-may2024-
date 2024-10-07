2350fca7f23ce0e291d7410a77b6a0f01ce1c7c5
Mohith Butta <124442398+mohithnovoct@users.noreply.github.com>
2024-10-07 12:29:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -223,4 +223,6 @@ def update_graph(selected_fits_file):
 
 # Run the Dash app
 if __name__ == '__main__':
-    app.run_server(debug=True)
+
+    port = int(os.environ.get('PORT', 8050))
+    app.run_server(host='0.0.0.0', port=port, debug=True)
